79d31d49272f1b76cee87c95f4a57b72165ecbb9
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-05-20 14:02:01 -0300

    feat: footer adjust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <Header />
         <CardContainer />
         <footer className="w-screen bg-black px-0 py-5 text-white mt-6 desktop:mt-0">
-          <div className="w-full desktop:w-4/4 mx-auto flex bg-transparent justify-between items-end px-6 gap-10 ">
+          <div className="w-full desktop:w-3/4 mx-auto flex bg-transparent justify-between items-end px-6 gap-10 ">
             <Image
               src="/img/logo_grupo.svg"
               width={70}
