4d4faa906b7b10411a50e997e9a09061c23fb8e4
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-05-22 11:36:49 -0300

    fix: Logo footer and css adjusts

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -5,15 +5,18 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <>
-      <footer className="w-full  bg-black px-0 py-5 text-white mt-6 relative laptop:absolute  desktop:absolute desktop:bottom-0">
+      <footer className="w-full  bg-black px-0 py-5 text-white mt-7 relative laptop:absolute  desktop:absolute desktop:bottom-0">
         <div className=" max-w-[90rem] desktop:w-[95%] mx-auto flex bg-transparent justify-start items-end px-6 desktop:px-0 tablet:justify-between gap-10 ">
-          <Image
-            src="/img/logo_grupo.svg"
-            width={126}
-            height={92}
-            alt="Logo grupo plan marketing"
-          />
-          <small className="">
+          <div className="relative w-[7.8rem] h-[5.7rem]">
+            <Image
+              fill
+              style={{ objectFit: 'contain' }}
+              priority
+              src="/img/logo_grupo.svg"
+              alt="Logo grupo plan marketing"
+            />
+          </div>
+          <small>
             Grupo Plan Marketing (C) Todos os direitos reservados - 2025
           </small>
         </div>
