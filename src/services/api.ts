1b6975e4be4bff67da00732531ce336b5dd63666
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-06-05 22:30:08 -0300

    fix: The API endpoints was changed, but I fix for the new format.

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,11 @@ import axios from 'axios'
 
 export async function getData() {
   try {
-    const response = await axios.get('https://restcountries.com/v3.1/all', {})
-
+    const response = await axios.get(
+      'https://restcountries.com/v3.1/all?fields=name,flags,continents,region,translations,capital,languages',
+      {},
+    )
+    console.log('Dados recebidos:', response.data)
     return response.data
   } catch (error) {
     console.error('Erro ao buscar dados:', error)
@@ -11,7 +14,9 @@ export async function getData() {
   }
 }
 export async function getCountryByName(name: string) {
-  const res = await fetch(`https://restcountries.com/v3.1/name/${name}`)
+  const res = await fetch(
+    `https://restcountries.com/v3.1/all?fields=${name},por,flags,continents,region,translations,capital,languages,name,population`,
+  )
 
   if (!res.ok) {
     throw new Error('Erro ao buscar país')
